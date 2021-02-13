c79f06897699cd471495fd01307166de97362883
Mark Ignacio <mark@ignacio.io>
2021-02-12 20:15:46 -0800

    add GetLiveParam()

--- goytchat.go
+++ goytchat.go
@@ -3,10 +3,22 @@ package goytchat
 import (
 	"bytes"
 	"encoding/base64"
+	"math/rand"
+	"time"
 )
 
-// GetArchivedParam gets the parameter string for live chat.
-func GetArchivedParam(videoID string, seekTime uint, topChatOnly bool, channelID string) string {
+func genHeader(videoID, channelID string) string {
+	s1_3 := rs(1, []byte(videoID))
+	s1_5 := append(rs(1, []byte(channelID)), rs(2, []byte(videoID))...)
+	s1 := append(rs(3, s1_3), rs(5, s1_5)...)
+	s3 := rs(48687757, rs(1, []byte(videoID)))
+	h := append(rs(1, s1), rs(3, s3)...)
+	h = append(h, nm(4, 1)...)
+	return base64.URLEncoding.EncodeToString(h)
+}
+
+// GetArchivedParam gets the parameter string for archived videos' live chat.
+func GetArchivedParam(videoID, channelID string, seekTime uint, topChatOnly bool) string {
 	var (
 		chatType  int
 		timestamp = seekTime * 1000000
@@ -16,14 +28,7 @@ func GetArchivedParam(videoID string, seekTime uint, topChatOnly bool, channelID
 	} else {
 		chatType = 1
 	}
-	// encode header
-	s1_3 := rs(1, []byte(videoID))
-	s1_5 := append(rs(1, []byte(channelID)), rs(2, []byte(videoID))...)
-	s1 := append(rs(3, s1_3), rs(5, s1_5)...)
-	s3 := rs(48687757, rs(1, []byte(videoID)))
-	h := append(rs(1, s1), rs(3, s3)...)
-	h = append(h, nm(4, 1)...)
-	header := base64.URLEncoding.EncodeToString(h)
+	header := genHeader(videoID, channelID)
 	// make the protobuf-lookin' thing
 	var buf = &bytes.Buffer{}
 	buf.Write(rs(3, []byte(header)))
@@ -37,4 +42,59 @@ func GetArchivedParam(videoID string, seekTime uint, topChatOnly bool, channelID
 	buf.Write(nm(15, 0))
 	continuation := rs(156074452, buf.Bytes())
 	return base64.URLEncoding.EncodeToString(continuation)
+}
+
+const liveTimeMult = 1000000
+
+// GetLiveParam gets the parameter string for live videos' live chat.
+func GetLiveParam(videoID, channelID string, pastSeconds uint, topChatOnly bool) string {
+	var chatType int
+	if topChatOnly {
+		chatType = 4
+	} else {
+		chatType = 1
+	}
+	head := genHeader(videoID, channelID)
+	// generate times
+	// TODO: is math/rand uniformly distributed by default?
+	now := float64(time.Now().Unix())
+	ts1 := int((now - randFloat64Between(0, 3)) * liveTimeMult)
+	ts2 := int((now - randFloat64Between(0.01, 0.99)) * liveTimeMult)
+	ts3 := int((now - float64(pastSeconds) + rand.Float64()) * liveTimeMult)
+	ts4 := int((now - randFloat64Between(600, 3600)) * liveTimeMult)
+	ts5 := int((now - randFloat64Between(0.01, 0.99)) * liveTimeMult)
+
+	// make the protobuf-lookin thing
+	var bodyBuf = &bytes.Buffer{}
+	bodyBuf.Write(nm(1, 0))
+	bodyBuf.Write(nm(2, 0))
+	bodyBuf.Write(nm(3, 0))
+	bodyBuf.Write(nm(4, 0))
+	bodyBuf.Write(rs(7, []byte{}))
+	bodyBuf.Write(nm(8, 0))
+	bodyBuf.Write(rs(9, []byte{}))
+	bodyBuf.Write(nm(10, ts2))
+	bodyBuf.Write(nm(11, 3))
+	bodyBuf.Write(nm(15, 0))
+	var entityBuf = &bytes.Buffer{}
+	entityBuf.Write(rs(3, []byte(head)))
+	entityBuf.Write(nm(5, ts1))
+	entityBuf.Write(nm(6, 0))
+	entityBuf.Write(nm(7, 0))
+	entityBuf.Write(nm(8, 1))
+	entityBuf.Write(rs(9, bodyBuf.Bytes()))
+	entityBuf.Write(nm(10, ts3))
+	entityBuf.Write(nm(11, ts4))
+	entityBuf.Write(nm(13, chatType))
+	entityBuf.Write(rs(16, nm(1, chatType)))
+	entityBuf.Write(nm(17, 0))
+	entityBuf.Write(rs(19, nm(1, 0)))
+	entityBuf.Write(nm(20, ts5))
+	continuation := rs(119693434, entityBuf.Bytes())
+	return base64.URLEncoding.EncodeToString(continuation)
+}
+
+func randFloat64Between(floor, ceiling float64) float64 {
+	diff := ceiling - floor
+	return (rand.ExpFloat64() * diff) + floor
 }
--- goytchat_test.go
+++ goytchat_test.go
@@ -5,8 +5,14 @@ import (
 	"testing"
 )
 
-// acquired from: unquote(pytchat.paramgen.arcparam.getparam("dWNvlyycWzQ", 0, False, "UCTUHzVzRwN_2x13IWQ9QVNg"))
-const okieDokieBoomer = "op2w0wSCARpsQ2pnYURRb0xaRmRPZG14NWVXTlhlbEVxSndvWVZVTlVWVWg2Vm5wU2QwNWZNbmd4TTBsWFVUbFJWazVuRWd0a1YwNTJiSGw1WTFkNlVSb1Q2cWpkdVFFTkNndGtWMDUyYkhsNVkxZDZVU0FCKAAwADgAQABIBFICIAByAggEeAA="
+// acquired from pytchat 0.5.3
+const (
+	// unquote(pytchat.paramgen.arcparam.getparam("dWNvlyycWzQ", 0, False, "UCTUHzVzRwN_2x13IWQ9QVNg"))
+	okieDokieBoomerArchived = "op2w0wSCARpsQ2pnYURRb0xaRmRPZG14NWVXTlhlbEVxSndvWVZVTlVWVWg2Vm5wU2QwNWZNbmd4TTBsWFVUbFJWazVuRWd0a1YwNTJiSGw1WTFkNlVSb1Q2cWpkdVFFTkNndGtWMDUyYkhsNVkxZDZVU0FCKAAwADgAQABIBFICIAByAggEeAA="
+	// random.seed(42)
+	// unquote(pytchat.paramgen.liveparam.getparam("dWNvlyycWzQ", "UCTUHzVzRwN_2x13IWQ9QVNg", 0, False))
+	okieDokieBoomerLive = "0ofMyAPFARpsQ2pnYURRb0xaRmRPZG14NWVXTlhlbEVxSndvWVZVTlVWVWg2Vm5wU2QwNWZNbmd4TTBsWFVUbFJWazVuRWd0a1YwNTJiSGw1WTFkNlVSb1Q2cWpkdVFFTkNndGtWMDUyYkhsNVkxZDZVU0FCKPfU3cD55e4CMAA4AEABShsIABAAGAAgADoAQABKAFDx0dDB-eXuAlgDeABQlcTjwfnl7gJY6cWe5PTl7gJoAYIBAggBiAEAmgECCACgAeKKpsH55e4C"
+)
 
 func TestVN(t *testing.T) {
 	if !bytes.Equal([]byte{byte(128), byte(4)}, vn(512)) {
@@ -15,8 +21,21 @@ func TestVN(t *testing.T) {
 }
 
 func TestGetArchivedParam(t *testing.T) {
-	b64 := GetArchivedParam("dWNvlyycWzQ", 0, false, "UCTUHzVzRwN_2x13IWQ9QVNg")
-	if b64 != okieDokieBoomer {
-		t.Fatalf("expected %s, got %s", okieDokieBoomer, b64)
+	b64 := GetArchivedParam("dWNvlyycWzQ", "UCTUHzVzRwN_2x13IWQ9QVNg", 0, false)
+	if b64 != okieDokieBoomerArchived {
+		t.Fatalf("expected %s, got %s", okieDokieBoomerArchived, b64)
+	}
+}
+func TestGetLiveParam(t *testing.T) {
+	// rand.Seed(42)
+	b64 := GetLiveParam("dWNvlyycWzQ", "UCTUHzVzRwN_2x13IWQ9QVNg", 0, false)
+	// match the 5 random parts.
+	b64 = b64[:157] + okieDokieBoomerLive[157:165] + b64[165:] // ts2
+	b64 = b64[:198] + okieDokieBoomerLive[198:206] + b64[206:] // ts1
+	b64 = b64[:216] + okieDokieBoomerLive[216:223] + b64[223:] // ts3
+	b64 = b64[:228] + okieDokieBoomerLive[228:236] + b64[236:] // ts4
+	b64 = b64[:261] + okieDokieBoomerLive[261:269] + b64[269:] // ts5
+	if b64 != okieDokieBoomerLive {
+		t.Fatalf("expected %s, got %s", okieDokieBoomerLive, b64)
 	}
 }
